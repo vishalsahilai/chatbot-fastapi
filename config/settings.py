1c559e71cc2904c2bb814dddeec54fe72b0e5548
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-25 04:02:44 +0500

    add singleton Settings instance for app-wide config access

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
-from typing import list
+from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -8,27 +8,41 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
-#LLM
-google_api_key: str = "changeme"
-gemini_model: str = "gemini-2.5-flast"
-llm_temperature: float = 0.7
-llm_max_tokens: int = 512 #tokens ≈ roughly 350-400 words
+    #LLM
+    google_api_key: str = "changeme"
+    gemini_model: str = "gemini-2.5-flash"
+    llm_temperature: float = 0.7
+    llm_max_tokens: int = 512 #tokens ≈ roughly 350-400 words
 
-#App
-#"development"	Running locally on your machine 
-#"staging"	Testing before going live
-#"production"	Live app for real users
-app_env: str = "development"
-app_host: str = "0.0.0.0"
-app_port: int = 8000
-debug: bool = True
+    #App
+    #"development"	Running locally on your machine 
+    #"staging"	Testing before going live
+    #"production"	Live app for real users
+    app_env: str = "development"
+    app_host: str = "0.0.0.0"
+    app_port: int = 8000
+    debug: bool = True
 
-#Memory
-memory_backend: str = "both"      # "dict" | "redis"
-max_summaries: int = 5
+    #Memory
+    memory_backend: str = "both"      # "dict" | "redis" | "both"
+    max_summaries: int = 5
 
-# Redis 
-redis_host: str = "localhost"
-redis_port: int = 6379
-redis_db: int = 0
-redis_password: str = ""
+    # Redis 
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    redis_db: int = 0
+    redis_password: str = ""
+
+
+
+    #CORS 
+    cors_origins: List[str] = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        ]
+
+
+# Singleton instance used across the app
+settings = Settings()
