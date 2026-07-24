6679f8f0267c22f03142b5a5a6701c77b1e8c8db
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-25 03:24:07 +0500

    configure Settings class with pydantic BaseSettings and env file

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,9 @@
 from typing import list
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8", #Read the .env file using UTF-8 encoding/ UTF-8 supports all characters including special symbols, emojis, other languages
+        case_sensitive=False,
+    )
