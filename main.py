c29766a87c050d522b0d494d8f64197ecdf933a2
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-25 01:31:13 +0500

    import custom logging utility from logger

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from config.settings import settings
-from routers.chat import chat_router
-from routers.health import health_router 
+from routers.chat import router as chat_router
+from routers.health import router as health_router 
+from utils.logger import logger 
