884e191cd7b4511a2c9ddc46bcfc4c1095a6b52e
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-25 14:33:41 +0500

    add restaurant menu dictionary with categories and items

---
+++ utils/menu.py
@@ -0,0 +1,24 @@
+MENU: dict = {
+    "pizza":[
+        "Margherita",
+        "Pepperoni",
+    ],
+    "burger": [
+        "Zinger Burger",
+        "Beef Burger",
+    ],
+    "drinks": [
+        "Coca-Cola",
+        "Mango Lassi",
+        "Mineral Water",
+    ],
+    "sides": [
+        "Garlic Bread",
+        "Coleslaw",
+        "French Fries",
+    ],
+    "desserts": [
+        "Chocolate Brownie",
+        "Gulab Jamun",
+    ],
+}
