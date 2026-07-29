76445e978b380a769a0969501bd595cba050e8df
Vishal Sahil <vishalsahilofficial@gmail.com>
2026-07-29 18:46:15 +0500

    import settings, system prompt and logger into LLM service

--- services/llm_service.py
+++ services/llm_service.py
@@ -3,3 +3,7 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from tenacity import retry, stop_after_attempt, wait_exponential, RetryError
+
+from config.settings import settings
+from prompts.system_prompt import SYSTEM_PROMPT
+from utils.logger import logger
