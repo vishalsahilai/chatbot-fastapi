6477c9d6fdc40aadaf40a15eceaa4f8cd59c8a23
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 12:34:06 -0700

    add collection helper functions for customers, sessions, and orders

--- database/models.py
+++ database/models.py
@@ -1,1 +1,11 @@
 from database.mongodb import get_db
+
+def customers_col():
+    return get_db()["customers"]
+
+def sessions_col():
+    return get_db()["sessions"]
+ 
+ 
+def orders_col():
+    return get_db()["orders"]
