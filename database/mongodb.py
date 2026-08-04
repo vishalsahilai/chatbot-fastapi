9c7937bb345ab8e5b02e673d6012c2719bd0e121
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 12:25:18 -0700

    add get_db function with singleton MongoDB connection

--- database/mongodb.py
+++ database/mongodb.py
@@ -6,3 +6,10 @@ from utils.logger import logger
 _client: MongoClient = None
 _db: Database = None
 
+def get_db() -> Database:
+    global _client, _db
+    if _db is None:
+        _client = MongoClient(settings.mongodb_url)
+        _db = _client["sadabahar"]
+        logger.info("Connected to MongoDB")
+    return _db
