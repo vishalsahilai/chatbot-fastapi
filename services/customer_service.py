e69eb1127878ee23ca5c0075fbbd682c62e5a1fb
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 12:54:58 -0700

    add get_customer function to fetch customer by phone

--- services/customer_service.py
+++ services/customer_service.py
@@ -1,3 +1,6 @@
 from datetime import datetime, timezone
 from database.models import customers_col
 from utils.logger import logger
+
+def get_customer(phone: str) -> dict | None:
+    return customers_col().find_one({"phone": phone}, {'_id':0})
