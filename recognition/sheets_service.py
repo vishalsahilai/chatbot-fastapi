e0909429f48f87a281867566e91da756af1a294d
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 14:08:31 -0700

    add _get_sheet helper to authenticate and access Google Sheet

--- recognition/sheets_service.py
+++ recognition/sheets_service.py
@@ -5,3 +5,9 @@ from config.settings import settings
 from utils.logger import logger
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+def _get_sheet():
+    creds_json = json.loads(settings.google_service_account_json)
+    creds = Credentials.from_service_account_info(creds_json, scopes=SCOPES)
+    client = gspread.authorize(creds)
+    return client.open_by_key(settings.google_sheets_id).sheet1
