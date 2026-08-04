3adfb0b772f4aa3ff77d6de89527d8ab44cd1813
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 13:54:34 -0700

    add generate_order_id function to create unique order IDs

--- routes/order.py
+++ routes/order.py
@@ -4,3 +4,8 @@ from services.customer_service import update_last_order
 from services.sheets_service import save_order_to_sheets
 from services.email_service import send_confirmation_email
 from utils.logger import logger
+
+def generate_order_id() -> str:
+    now = datetime.now(timezone.utc)
+    count = orders_col().count_documents({})
+    return f"ORD-{now.strftime('%Y%m%d')}-{str(count + 1).zfill(3)}"
