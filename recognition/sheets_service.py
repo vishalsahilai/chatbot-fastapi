203716271b1c7c9eb444e9b3b629c9aa0564361f
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 14:04:22 -0700

    define Google Sheets API scope for service account

--- recognition/sheets_service.py
+++ recognition/sheets_service.py
@@ -3,3 +3,5 @@ import gspread
 from google.oauth2.service_account import Credentials
 from config.settings import settings
 from utils.logger import logger
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
