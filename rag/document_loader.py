efc29a98364d8191083493d38539891c59096bd0
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-04 09:00:33 -0700

    fix import of pyPDFloader

--- rag/document_loader.py
+++ rag/document_loader.py
@@ -1,5 +1,5 @@
 import os
-from langchain.comunity.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from utils.logger import logger
 
