5a7c481ddde8b6d8aa7e29a11d5fb64de94f0a68
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-05 03:18:06 -0700

    add class of order items

--- routes/order.py
+++ routes/order.py
@@ -4,3 +4,9 @@ from typing import List
 from services.order_service import process_order
 
 router = APIRouter()
+
+class OrderItem(BaseModel):
+    name: str
+    size: str = ""
+    qty: int
+    price: float
